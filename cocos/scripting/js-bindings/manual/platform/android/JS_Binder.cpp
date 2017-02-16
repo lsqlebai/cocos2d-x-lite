3cb6b5ea7504475c32128819cc7fe4f91046e3d2
乐百 <lsqlebai@163.com>
2017-02-16 10:42:07 +0800

    1. update external-config.json 2.fix file to utf-8 3. merge code and fix bugs

--- cocos/scripting/js-bindings/manual/platform/android/JS_Binder.cpp
+++ cocos/scripting/js-bindings/manual/platform/android/JS_Binder.cpp
@@ -2,13 +2,10 @@
 #include"scripting/js-bindings/manual/iflytek/CallbackManager.h"
 #include"cocos2d.h"
 using namespace cocos2d;
-void runOnCocosThread(const std::function<void()>& fun)
-{
-	Director::getInstance()->getScheduler()->performFunctionInCocosThread(fun);
-}
+
 
 bool JSBinder::callJS(std::string domain, std::string arg) {
-	runOnCocosThread( [=]() {
+	Director::getInstance()->getScheduler()->performFunctionInCocosThread([=]() {
 		CallbackManager::getInstance()->callJS(domain, arg); 
 	});
 	return true;
