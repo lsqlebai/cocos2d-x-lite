5bbe821fe658f3e7ad350aba0e350b93a8ff68fe
592672849@qq.com <592672849@qq.com>
2017-04-26 10:26:56 +0800

    修改了模板

--- templates/js-template-link/frameworks/runtime-src/proj.android-studio/app/src/com/iflytek/leagueofglutton/AppActivity.java
+++ templates/js-template-link/frameworks/runtime-src/proj.android-studio/app/src/com/iflytek/leagueofglutton/AppActivity.java
@@ -24,21 +24,36 @@ THE SOFTWARE.
 ****************************************************************************/
 package com.iflytek.leagueofglutton;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.iflytek.unipay.PayComponent;
 import com.iflytek.unipay.js.CocoActivityHelper;
+import com.iflytek.utils.common.ApkUtil;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 
 // For JS and JAVA reflection test, you can delete it if it's your own project
 
 public class AppActivity extends Cocos2dxActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ApkUtil.init(this);
 
         CocoActivityHelper.setActivity(this);
         PayComponent.getInstance().init(this);
@@ -52,5 +67,134 @@ public class AppActivity extends Cocos2dxActivity {
 
         return glSurfaceView;
     }
+
+    public static void copyFile(File srcFile, File destFile) throws IOException {
+        if (!destFile.exists()) {
+            destFile.createNewFile();
+        }
+
+        FileChannel source = null;
+        FileChannel destination = null;
+
+        try {
+            source = new FileInputStream(srcFile).getChannel();
+            destination = new FileOutputStream(destFile).getChannel();
+            destination.transferFrom(source, 0, source.size());
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            if (source != null) {
+                source.close();
+            }
+            if (destination != null) {
+                destination.close();
+            }
+        }
+    }
+
+    /**
+     * 拷贝so库
+     */
+    private void copyNewLib()
+    {
+        File libCopyDirFile = new File(getExternalFilesDir("download").getAbsolutePath() + "/libCopyDir");
+        if(libCopyDirFile.exists() && libCopyDirFile.isFile())
+        {
+            BufferedReader br =  null;
+            try {
+                br =  new BufferedReader(new FileReader(libCopyDirFile));
+                String filePath = br.readLine();
+                File libDir = new File(filePath);
+                if(libDir.exists() && libDir.isDirectory())
+                {
+                    File[] soFiles = libDir.listFiles(new FileFilter() {
+                        @Override
+                        public boolean accept(File pathname) {
+                            return pathname.getName().endsWith(".so");
+                        }
+                    });
+
+                    if(null != soFiles && soFiles.length>0)
+                    {
+                        File dstDir = getFilesDir();
+                        SharedPreferences sp = getPreferences(MODE_PRIVATE);
+
+                        for(File soFile : soFiles)
+                        {
+                            String key = "KEY_LIB_LAST_TIME_" + soFile.getName();
+
+                            File dstFile = new File(dstDir.getAbsolutePath() + "/" + soFile.getName());
+
+                            long lastTime = sp.getLong(key, 0);
+                            if(!dstFile.exists() || dstFile.length() != soFile.length() || lastTime != soFile.lastModified()) // so库发生过变化，则进行覆盖
+                            {
+                                copyFile(soFile, dstFile);
+                                sp.edit().putLong(key, soFile.lastModified()).commit(); // 保存本次拷贝库的时间
+
+                            }
+                        }
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(null != br)
+                {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private void copyOriLib()
+    {
+        File srcDir = new File("/data/data/" + getPackageName() + "/lib");
+        File dstDir = getFilesDir();
+
+        File[] srcFiles = srcDir.listFiles();
+        if(null != srcFiles)
+        {
+            for(File srcFile : srcFiles)
+            {
+
+                File dstFile = new File(dstDir.getAbsolutePath() + "/" + srcFile.getName());
+
+                if(!dstFile.exists())
+                {
+                    try {
+                        copyFile(srcFile, dstFile);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onLoadNativeLibraries() {
+
+        copyOriLib();
+
+        copyNewLib();
+
+        //super.onLoadNativeLibraries();
+
+        try {
+            System.load(getFilesDir() + "/libiflyteknet.so");
+            System.load(getFilesDir() + "/libcocos2djs.so");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
