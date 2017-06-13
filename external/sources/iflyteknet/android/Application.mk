d0d3085f175ecc6bf46b8cf258f7a82c2a6c1214
592672849@qq.com <592672849@qq.com>
2017-06-13 10:12:17 +0800

    修改编译参数

--- external/sources/iflyteknet/android/Application.mk
+++ external/sources/iflyteknet/android/Application.mk
@@ -15,6 +15,7 @@ ifeq ($(NDK_DEBUG),1)
   APP_CPPFLAGS += -DCOCOS2D_DEBUG=1
   APP_OPTIM := debug
 else
+  APP_CPPFLAGS += -DCOCOS2D_DEBUG=1
   APP_CPPFLAGS += -DNDEBUG
   APP_OPTIM := release
 endif
